aa19d40ec8fd6d7ca5a09f3b3d65489989fd965e
gupta-gopal304 <116249470+gupta-gopal304@users.noreply.github.com>
2023-03-10 12:15:27 +0530

    Update BrowserStackSample.js

--- ios/BrowserStackSample.js
+++ ios/BrowserStackSample.js
@@ -2,58 +2,27 @@ let wd = require('wd');
 let assert = require('assert');
 let asserters = wd.asserters;
 let Q = wd.Q;
-
 desiredCaps = {
   // Set your BrowserStack access credentials
-  'browserstack.user' : 'YOUR_USERNAME',
-  'browserstack.key' : 'YOUR_ACCESS_KEY',
-
-    // Set URL of the application under test
-  'app' : 'bs://<app-id>',
-
+  'browserstack.user' : 'gopalgupta_2rzYBm',
+  'browserstack.key' : 'F5weLDx6NJMiHwepy2cy',
+    // Set app_url of the application under test
+  'app' : 'bs://d9997845b0a9819bd16e32779fe894485777a34d',
   // Specify device and os_version for testing
-  'device' : 'iPhone 11 Pro',
-  'os_version' : '13',
-
+  'device' : 'Google Pixel 6',
+  'os_version' : '12.0',
   // Set other BrowserStack capabilities
   'project' : 'First NodeJS project',
-  'build' : 'Node iOS',
+  'build' : 'browserstack-build-1',
   'name': 'first_test'
 };
-
 // Initialize the remote Webdriver using BrowserStack remote URL
 // and desired capabilities defined above
 driver = wd.promiseRemote("http://hub-cloud.browserstack.com/wd/hub");
-
 // Test case for the BrowserStack sample iOS app. 
 // If you have uploaded your app, update the test case here.
 driver.init(desiredCaps)
-  .then(function () {
-    return driver.waitForElementById('Text Button', asserters.isDisplayed 
-    && asserters.isEnabled, 30000);
-  })
-  .then(function (textButton) {
-    return textButton.click();
-  })
-  .then(function () {
-    return driver.waitForElementById('Text Input', asserters.isDisplayed 
-    && asserters.isEnabled, 30000);
-  })
-  .then(function (textInput) {
-    return textInput.sendKeys("hello@browserstack.com"+"\n");
-  })
-  .then(function () {
-    return driver.waitForElementById('Text Output', asserters.isDisplayed 
-    && asserters.isEnabled, 30000);
-  })
-  .then(function (textOutput) {
-    return textOutput.text().then(function(value) {
-      if (value === "hello@browserstack.com")
-        assert(true);
-      else
-        assert(false);
-    });
-  })
+  //Write your custom code here
   .fin(function() { 
     // Invoke driver.quit() after the test is done to indicate that the test is completed.
     return driver.quit(); 
